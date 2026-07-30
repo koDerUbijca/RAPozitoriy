afba78b1424f5239a8b35c56132d839b9dc02fe8
Budjakyj <ares46666@gmail.com>
2026-07-30 23:13:37 +0200

    commit message

--- task4/task4/Program.cs
+++ task4/task4/Program.cs
@@ -4,10 +4,3 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Xml.Linq;
 using static System.Runtime.InteropServices.JavaScript.JSType;
-//1.створити функцію сей хелоу  
-void SayHello()
-{
-    Console.WriteLine("Hello, world"); 
-}
-
-SayHello();
